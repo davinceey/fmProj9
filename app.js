07e33a1112fee8407c1554f02513c29ce7430f87
Daniel Vincent <danielvincent940@gmail.com>
2022-09-20 15:12:31 +0100

    Final Commit

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
-const buttons = document.querySelectorAll(".btn");
-const response = document.querySelectorAll(".answer");
+const faq = document.querySelector(".faq")
 
-for(let button of buttons){
-    for(let res of response){
-        button.addEventListener("click", () => {
-            res.classList.toggle("displayed-answer");
-        });
-      }
+faq.addEventListener("click", (event)=>{
+    if(event.target.parentElement.parentElement.hasAttribute("data-toggle") && event.target.className === "arrow"){
+        const faqQuestion = event.target.parentElement.parentElement;
+        const answer = faqQuestion.querySelector(".collapsed--answer");
+        const question = faqQuestion.querySelector("p");
+        if(faqQuestion.classList.contains("active")){
+            closeAnswer();
+        } else{
+            if(faq.querySelector(".faq-questions.active")){
+                closeAnswer();
+            } 
+        question.style.color = "hsl(238, 29%, 16%)";
+        question.style.fontWeight = 700;
+        faqQuestion.classList.add("active");
+        answer.style.maxHeight = answer.scrollHeight + "px"; 
+    }
+}
+})
+
+function closeAnswer(){
+    faq.querySelector(".faq-questions.active .collapsed--answer").removeAttribute("style");
+    faq.querySelector(".faq-questions.active p").removeAttribute("style");
+    faq.querySelector(".faq-questions.active").classList.remove("active");
 }
